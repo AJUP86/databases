66ede511d168cc9f384ad7ba1b6d9b0b109f8dda
Alejandro Urroz <alejandrourroz86@gmail.com>
2021-10-25 01:25:05 +0200

    Solved ex2 week 3 databases

---
+++ Week3/homework_week3/ex2/transaction.js
@@ -0,0 +1,33 @@
+import { createConnection } from "mysql";
+import { promisify } from "util";
+
+const connection = createConnection({
+  host: "localhost",
+  user: "hyfuser",
+  password: "hyfpassword",
+  database: "Transactions",
+});
+const excQuery = promisify(connection.query.bind(connection));
+
+async function performSqlTransaction() {
+  const startTransaction = `START transaction;`;
+  const endTransaction = `commit;`;
+  const payMoney = `INSERT INTO account_change VALUES (3, 101, 1000, '2021-10-26 09:30:10', "Payment to my friend 102" );`;
+  const payerAccount = `UPDATE account SET balance = balance - 1000 WHERE account_number = 101; `;
+  const beneficiaryAccount = `UPDATE account SET balance = balance + 1000 WHERE account_number = 102;`;
+  const mistake = `ROLLBACK`;
+  connection.connect();
+  try {
+    await excQuery(startTransaction);
+    await excQuery(payMoney);
+    await excQuery(payerAccount);
+    await excQuery(beneficiaryAccount);
+    await excQuery(endTransaction);
+  } catch (error) {
+    console.error(error);
+    await excQuery(mistake);
+    connection.end();
+  }
+  connection.end();
+}
+performSqlTransaction();
---
+++ Week3/homework_week3/ex2/transactions-insert-values.js
@@ -0,0 +1,40 @@
+import { createConnection } from "mysql";
+
+const connection = createConnection({
+  host: "localhost",
+  user: "hyfuser",
+  password: "hyfpassword",
+  database: "Transactions",
+});
+
+async function insertValues() {
+  const accountQuery = `INSERT INTO account VALUES ?`;
+  const accountValues = [
+    [101, 3576.46],
+    [102, 432.32],
+  ];
+  const accountChangesQuery = `INSERT INTO account_change VALUES ? `;
+  const accountChangesValues = [
+    [1, 101, 1000.0, "2021-10-25 22:40:35", "Energy payment"],
+    [2, 102, 50.0, "2021-10-10 11:30:03", "restaurant bill"],
+  ];
+  connection.connect();
+  try {
+    await Promise.all[
+      (connection.query(accountQuery, [accountValues], (error, results) => {
+        error ? error : console.log(JSON.stringify(results));
+      }),
+      connection.query(
+        accountChangesQuery,
+        [accountChangesValues],
+        (error, results) => {
+          error ? error : console.log(JSON.stringify(results));
+        }
+      ))
+    ];
+  } catch (error) {
+    console.error(error);
+  }
+  connection.end();
+}
+insertValues();
